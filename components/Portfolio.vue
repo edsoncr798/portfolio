6a9c09e2d0a304d324a4ee396b186a4e4756221d
user.email <edsoncr798@gmail.com>
2024-09-06 12:35:09 -0500

    avance de portafolio section portafolio: -proyectos

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import {Swiper, SwiperSlide} from "swiper/vue";
-import "swiper/swiper-bundle.css";
 
-import bipbip from '~/static/images/jpg/bipbip.jpg'
-import sici from '~/static/images/jpg/sici.jpg'
-import pokedex from '~/static/images/jpg/pokedex.jpg'
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+import bipbip from '~/static/images/jpg/bipbip.jpg';
+import sici from '~/static/images/jpg/sici.jpg';
+import pokedex from '~/static/images/jpg/pokedex.jpg';
+import {Autoplay, Navigation, Pagination} from "swiper/modules";
+
+const modules = [Autoplay, Pagination, Navigation]
 
 const images = [
   {src: bipbip, alt: 'Project 1'},
   {src: sici, alt: 'Project 2'},
-  {src: pokedex, alt: 'Project 3'}
+  {src: pokedex, alt: 'Project 3'},
+  {src: pokedex, alt: 'Project 3'},
+  {src: sici, alt: 'Project 3'},
+  {src: pokedex, alt: 'Project 3'},
+  {src: bipbip, alt: 'Project 3'}
 ];
 
 const currentIndex = ref(0);
@@ -39,28 +49,17 @@ const prev = () => {
         En esta sección, te presento algunos de los proyectos que más me marcaron, tanto en mi etapa de formación como
         en mi carrera profesional. Estos proyectos reflejan mi capacidad para resolver problemas, aprender rápidamente
         nuevas herramientas, y contribuir efectivamente al éxito de un equipo.</p>
-      <!--      <div class="carousel grid grid-cols-3 grid-flow-row gap-20">-->
-      <!--        <div class="w-[100px] h-[150px]">-->
-      <!--          <img :src="pokedex" class=" h-full w-full rounded-lg" alt="screnpoke">-->
-      <!--        </div>-->
 
-      <!--        <div class="w-[100px] h-[150px]">-->
-      <!--          <img :src="bipbip" class=" h-full w-full rounded-lg" alt="scren-bipbip">-->
-      <!--        </div>-->
-
-      <!--        <div class="w-[100px] h-[150px]">-->
-      <!--          <img :src="sici" class=" h-full w-full rounded-lg" alt="screnSici">-->
-      <!--        </div>-->
-
-      <!--        <div class="w-[100px] h-[150px]">-->
-      <!--          <img :src="pokedex" class=" h-full w-full rounded-lg" alt="img">-->
-      <!--        </div>-->
-
-      <!--      </div>-->
-
-      <swiper :slides-per-view="1" navigation pagination>
-        <swiper-slide v-for="(image, index) in images" :key="index">
-          <img :src="image.src" :alt="image.alt"/>
+      <swiper
+          ref="{swiperRef}"
+          :slidesPerView="3"
+          :spaceBetween="10"
+          :pagination="true"
+          :modules="modules"
+          :autoplay="{delay: 3000}"
+          class="mySwiper">
+        <swiper-slide class="rounded-lg h-[200px] md:h-[500px]" v-for="(image, index) in images" :key="index">
+          <img class="rounded-lg" :src="image.src" :alt="image.alt"/>
         </swiper-slide>
       </swiper>
 
@@ -69,10 +68,26 @@ const prev = () => {
 </template>
 
 <style scoped>
-img {
-  width: 100%;
-  height: auto;
-  display: block;
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: fill;
+  cursor: pointer;
+}
+
+
 
 </style>
